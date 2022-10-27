4cb5bc9f722df7873361d02f078d001967ffe846
erikx50 <erikx50@users.noreply.github.com>
2022-10-27 14:45:35 +0200

    Baseline results now write to file

--- manual_baseline.py
+++ manual_baseline.py
@@ -12,6 +12,15 @@ def load_json(filepath):
         return f
 
 
+def write_results(path, result):
+    with open(path, "w") as file:
+        for id in result:
+            counter = 1
+            for doc in result[id]:
+                file.write(str(id) + ' ' + 'Q0' + ' ' + str(doc) + ' ' + str(counter) + ' ' + str(result[id][doc]) + '\n')
+                counter += 1
+
+
 def baseline_retrieval(es, index_name, query, k):
     query_list = []
     for term in query:
@@ -25,10 +34,10 @@ def baseline_retrieval(es, index_name, query, k):
         }
 
     res = es.search(index=index_name, body=q, size=k)       # Receives top k candidates in descending order
-    return_list = []
+    return_dict = {}
     for hits in res['hits']['hits']:
-        return_list.append(hits['_id'])
-    return return_list
+        return_dict[hits['_id']] = hits['_score']
+    return return_dict
 
 
 def run_baseline_retrieval(es, eval_topics, index_name, k):
@@ -42,14 +51,16 @@ def run_baseline_retrieval(es, eval_topics, index_name, k):
 
 if __name__ == "__main__":
     # Initiate elastic search client
-    es = Elasticsearch()
-
+    es = Elasticsearch(timeout=120)
     # Import train data
     manual_train_data = load_json(os.path.normpath('2020/2020_manual_evaluation_topics_v1.0.json'))
 
     # Run baseline retrieval
     result = run_baseline_retrieval(es, manual_train_data, INDEX_NAME, 500)
-    print(result)
+    print(result['81_1'])
+
+    # Write result to file
+    write_results(os.path.normpath('results/manual_results.txt'), result)
 
     # TODO: Remove code under
-    #print(es.get(index="collection", id='MARCO_335000')['_source'])
+    # print(es.get(index="collection", id='MARCO_570000')['_source'])
