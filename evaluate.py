f7aa84884babfc1aa084bf9755bd45cb1141a63e
erikx50 <erikx50@users.noreply.github.com>
2022-10-28 11:50:14 +0200

    Switched to ir_measures to calculate scores

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,12 @@
-from trectools import TrecRun, TrecQrel, TrecEval
 import os
+import ir_measures
+from ir_measures import nDCG, MAP, MRR, R
 
 
 def evaluate(result_path, qrels_path):
-    run = TrecRun(result_path)
-    qrels = TrecQrel(qrels_path)
-    eval = TrecEval(run, qrels)
-    print('Evaluation of run')
-    print('Recall: ', )
-    print('MAP: ' + str(eval.get_map(depth=1000)))
-    print('MRR: ' + str(eval.get_reciprocal_rank(depth=1000)))
-    print('NDCG: ' + str(eval.get_ndcg(depth=1000)))
-    print('NDCG@3: ' + str(eval.get_ndcg(depth=3)))
-
+    qrels = ir_measures.read_trec_qrels(qrels_path)
+    run = ir_measures.read_trec_run(result_path)
+    print(ir_measures.calc_aggregate([R@1000, MAP@1000, MRR@1000, nDCG@1000, nDCG@3], qrels, run))
 
 
 if __name__ == "__main__":
